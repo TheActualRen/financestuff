2504252f8b8e415e1a6a9e7ec41093493087dbc0
TheActualRen <125380929+TheActualRen@users.noreply.github.com>
2025-01-27 19:34:44 +0000

    Update market_data.py

--- market_data.py
+++ market_data.py
@@ -1,7 +1,5 @@
 import yfinance as yf
 import pandas as pd
-import numpy as np
-from datetime import datetime
 
 stock = yf.Ticker("AAPL")
 from_date = "2022-01-01"
@@ -12,9 +10,6 @@ open_values = stock_data["Open"].tolist()
 close_values = stock_data["Close"].tolist()
 low_values = stock_data["Low"].tolist()
 
-# The first way
-dates = stock_data.index.strftime('%Y-%m-%d').tolist()
-unix_timestamps = [int(datetime.strptime(date, '%Y-%m-%d').timestamp()) for date in dates]
-
-# Defo Better (Sea of Tranquility) 
-dates_straight_to_timestamps = [int(ts / 10**9) for ts in stock_data.index.astype(int)]
+dates = stock_data.index.tolist()
+unix_timestamps = [int(timestamp.timestamp()) for timestamp in dates]
+print("Unix Timestamps:", unix_timestamps)
